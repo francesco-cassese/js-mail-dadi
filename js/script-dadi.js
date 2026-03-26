36fedbbb5d9635059c7091eac5be6b6da2d232a6
Francesco Cassese <francescocassese9@gmail.com>
2026-03-26 19:08:06 +0100

    aggiunta logica della partita con i dadi  a più round

--- js/script-dadi.js
+++ js/script-dadi.js
@@ -1,5 +1,39 @@
 'use script';
 
-//------ CREO LA LISTA DEI NUMERI-------
+//------ Variabili  -------
+let player;
+let computer;
+const playerScore = [];
+const computerScore = [];
+let numRound = 5;
+let totComputer = 0;
+let totPlayer = 0;
 
-const numberList = [1, 2, 3, 4, 5, 6];
+// ---- Ciclo per salvare i risultati del player ----
+for (let i = 0; i <= numRound; i++) {
+    player = Math.floor(Math.random() * 6) + 1;
+    playerScore.push(player);
+    totPlayer += playerScore[i];
+}
+
+// ---- Ciclo per salvare i risultati del player ----
+
+for (let i = 0; i <= numRound; i++) {
+    computer = Math.floor(Math.random() * 6) + 1;
+    computerScore.push(computer);
+    totComputer += computerScore[i];
+}
+
+// ---- Stampo i risultati sommati ----
+
+console.log(`Player: ${totPlayer} Computer: ${totComputer}`)
+
+// ---- Condizioni per il messaggio finale ----
+console.log(
+    totPlayer > totComputer ? ` complimenti hai vinto` :
+        totPlayer < totComputer ? `il computer ti ha battuto` :
+            totPlayer === totComputer ? `Ritenta, avete pareggiato` : `Qualcosa è andato storto`
+);
+
+
+
