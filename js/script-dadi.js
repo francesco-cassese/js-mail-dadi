8360ba1a2dd6fb0e392f9c2e79de335a6daa5e9a
Francesco Cassese <francescocassese9@gmail.com>
2026-03-26 19:25:33 +0100

    Aggiunto ulteriore console.log ai for per vedere il numero esatto uscito ad ogni round

--- js/script-dadi.js
+++ js/script-dadi.js
@@ -10,18 +10,20 @@ let totComputer = 0;
 let totPlayer = 0;
 
 // ---- Ciclo per salvare i risultati del player ----
-for (let i = 0; i <= numRound; i++) {
+for (let i = 0; i < numRound; i++) {
     player = Math.floor(Math.random() * 6) + 1;
     playerScore.push(player);
     totPlayer += playerScore[i];
+    console.log(`Round ${i + 1} player: è uscito il numero ${player}`);
 }
 
 // ---- Ciclo per salvare i risultati del player ----
 
-for (let i = 0; i <= numRound; i++) {
+for (let i = 0; i < numRound; i++) {
     computer = Math.floor(Math.random() * 6) + 1;
     computerScore.push(computer);
     totComputer += computerScore[i];
+    console.log(`Round ${i + 1} computer: è uscito il numero ${computer}`);
 }
 
 // ---- Stampo i risultati sommati ----
@@ -30,9 +32,9 @@ console.log(`Player: ${totPlayer} Computer: ${totComputer}`)
 
 // ---- Condizioni per il messaggio finale ----
 console.log(
-    totPlayer > totComputer ? ` complimenti hai vinto` :
-        totPlayer < totComputer ? `il computer ti ha battuto` :
-            totPlayer === totComputer ? `Ritenta, avete pareggiato` : `Qualcosa è andato storto`
+    totPlayer > totComputer ? alert(` complimenti hai vinto`) :
+        totPlayer < totComputer ? alert(`il computer ti ha battuto`) :
+            totPlayer === totComputer ? alert(`Ritenta, avete pareggiato`) : alert(`Qualcosa è andato storto`)
 );
 
 
