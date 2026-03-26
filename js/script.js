46474abba55963eafebe48e244f82db2ce6e3d86
Francesco Cassese <francescocassese9@gmail.com>
2026-03-26 14:37:33 +0100

    Creazione lista invitati e PseudoCodice iniziale

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+//Creo la lista degli invitati 
+//Chiedo all'utente di scrivere la propria Email
+//Controllo che quella inserita sia effettivamente valida
+//Controllo che l'email sia presente nella lista
+//SE l'utente è presente stampa Benvenuto nome utente
+
+//-------- CREAZIONE LISTA INVITATI ---------//
+
+const invitedUsers = [
+    'marco.rossi85@gmail.com',
+    'elena.bianchi.92@outlook.it',
+    'alessandro_verdi_77@yahoo.it',
+    'giulia.ferrari.napoli@libero.it',
+    'stefano.romano.work@protonmail.com',
+    'francesca_esposito89@tin.it',
+    'riccardo.gallo.94@icloud.com',
+    'valeria.fontana_2023@fastwebnet.it',
+    'matteo.rizzo.dev@virgilio.it',
+    'sofia.contini_88@alice.it'];
